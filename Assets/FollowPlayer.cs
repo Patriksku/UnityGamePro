54ef8a72f2e73ad814768d2d5aceaf3273391c78
Patriksku <patrikskuza@gmail.com>
2020-05-11 11:53:06 +0200

    Initial Commit
    Base for the game

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset; // holds three floats, (x,y,z)
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform is linked to the transform-component of the camera, as camera is the object inheriting the script.
+        transform.position = (player.position + offset); //the camera position is the same as the player. other +- values are altered directly in Unity.
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float playerForwardSpeed = 1000f;
+    public float playerSidewaySpeed = 300f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // FixedUpdate for physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, playerForwardSpeed * Time.deltaTime); // Preliminary - Constant force forward to move the player.
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-playerSidewaySpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(playerSidewaySpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
